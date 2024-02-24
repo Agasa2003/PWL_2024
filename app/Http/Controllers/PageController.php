4677995c3936fe99bed2922c99c971edc04dba5b
Ananda Galang Saputra <saputraananda343@gmail.com>
2024-02-24 23:00:04 +0700

    app/Http/Controllers/PageController.php

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return 'Nama : Ananda Galang Saputra dan NIM : 2141762124';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/world', function () {
+ return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+   });
+
+   Route::get('/about', function () {
+    return 'NIM : 2141762124 dan Nama : Ananda Galang Saputra';
+   });
+
+
+Route::get('/user/{Galang?}', function ($name='Galang') {
+    return 'Nama Saya : '.$name;
+});
+
+Route::get('/posts/{posts}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-: 1'.$postId."Komentar ke-: 1 ".$commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return "Halaman Artikel dengan ID : ".$id;
+});
+
+Route::get('/about/{index}', function($index){
+    return "Selamat Datang Di Pemograman Web Lanjut ".$index;
 });
